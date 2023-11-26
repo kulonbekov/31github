be070500e4cd1dba3ac0b997c39f23623893ce1c
Kulonbekov <kulonbekov@gmail.com>
2023-11-26 20:29:19 +0600

    Create TodoService

---
+++ springrestapi/src/main/java/com/kulonbekov/springrestapi/service/TodoService.java
@@ -0,0 +1,4 @@
+package com.kulonbekov.springrestapi.service;
+
+public interface TodoService {
+}
---
+++ springrestapi/src/main/java/com/kulonbekov/springrestapi/service/impl/TodoServiceImpl.java
@@ -0,0 +1,8 @@
+package com.kulonbekov.springrestapi.service.impl;
+
+import com.kulonbekov.springrestapi.service.TodoService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoServiceImpl implements TodoService {
+}
