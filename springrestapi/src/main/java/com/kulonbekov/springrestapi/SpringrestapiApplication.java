6f290da867fc5dbd7965db649e91fe1d45548364
Kulonbekov <kulonbekov@gmail.com>
2023-12-10 12:38:41 +0600

    2. Добавил проверку

--- springrestapi/src/main/java/com/kulonbekov/springrestapi/SpringrestapiApplication.java
+++ springrestapi/src/main/java/com/kulonbekov/springrestapi/SpringrestapiApplication.java
@@ -1,5 +1,7 @@
 package com.kulonbekov.springrestapi;
 
+import com.declination.names.declinationnamesrussionandkyrgyz.Declination;
+import com.declination.names.declinationnamesrussionandkyrgyz.DeclinationProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,16 @@ public class SpringrestapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringrestapiApplication.class, args);
+
+		Declination name = new Declination("Кулонбеков Таалайбек Толосунович");
+
+		System.out.println(name.fullName(DeclinationProcessor.gcaseIm));
+		System.out.println(name.fullName(DeclinationProcessor.gcaseDat));
+		System.out.println(name.fullName(DeclinationProcessor.gcasePred));
+		System.out.println(name.fullName(DeclinationProcessor.gcaseRod));
+		System.out.println(name.fullName(DeclinationProcessor.gcaseTvor));
+		System.out.println(name.fullName(DeclinationProcessor.gcaseVin));
+
 	}
 
 }
